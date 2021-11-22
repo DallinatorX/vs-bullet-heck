145dc8c25224051c8bc894bf1b4f8901584facc2
Dallin <dallinhatton@gmail.com>
2021-11-22 11:57:42 -0700

    These are for the core of the game no new code was added

---
+++ bullet-heck/game/cross_pattern_fire.py
@@ -0,0 +1,5 @@
+from game.action import Action
+
+class cross_fire_pattern(Action):
+    def __init__(self):
+        super().__init__()
---
+++ bullet-heck/game/fire_bullet.py
@@ -0,0 +1,5 @@
+from game.action import Action
+
+class Fire_Bullet(Action):
+    def __init__(self):
+        super().__init__()
