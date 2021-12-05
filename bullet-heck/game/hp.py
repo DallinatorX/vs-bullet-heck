3029a83c6817cc6ef25303fb724ddcc2bf8ec819
Dallin <dallinhatton@gmail.com>
2021-12-04 19:12:46 -0700

    Added bullet damage and Game overs

---
+++ bullet-heck/game/hp.py
@@ -0,0 +1,13 @@
+from game.actor import Actor
+from game import constants
+
+class Hp(Actor):
+    def __init__(self):
+        super().__init__()
+        self.set_type("Hp")
+        self.set_text(f"HP: {constants.P2_HP}")
+
+    def set_Hp(self, hp):
+        self.set_text(f"HP: {hp}")  
+
+        
